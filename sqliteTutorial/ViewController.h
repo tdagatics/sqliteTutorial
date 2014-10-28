e96af9aeefa50922df48aa9aefef9f17f5def5ad
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-28 16:59:43 -0700

    Initial Commit

---
+++ sqliteTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sqliteTutorial
+//
+//  Created by Anthony Dagati on 10/28/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sqliteTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sqliteTutorial
+//
+//  Created by Anthony Dagati on 10/28/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
