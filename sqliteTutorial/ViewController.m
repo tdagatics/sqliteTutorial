1e08f731f0b86a8e6311ffcfbef8ca2b4ce671a0
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-29 10:05:18 -0700

    Final commit of SQLiteTutorial with questions

--- sqliteTutorial/ViewController.m
+++ sqliteTutorial/ViewController.m
@@ -46,25 +46,22 @@
             const char *sql_stmt = "CREATE TABLE IF NOT EXISTS PERSON (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, AGE INTEGER)";
             sqlite3_exec(personDB, sql_stmt, NULL, NULL, &error);
             sqlite3_close(personDB);
+            NSLog(@"Database created.");
             
         }
     }
 }
 
-- (IBAction)addButton:(id)sender {
-    char *error;
+- (IBAction) addButton:(id)sender
+{
     
+    char *error;
     if (sqlite3_open([dbPathString UTF8String], &personDB) == SQLITE_OK) {
-        NSString *insertStmt = [NSString stringWithFormat:@"INSERT INTO PERSONS(NAME, AGE) values ('%s' '%d')", [self.nameField.text UTF8String], [self.ageField.text intValue]];
+        NSString *insertStmt = [NSString stringWithFormat:@"INSERT INTO PERSONS (NAME, AGE) values ('%s' '%d')", [self.nameField.text UTF8String], [self.ageField.text intValue]];
         
         const char *insert_stmt = [insertStmt UTF8String];
         
-        if (sqlite3_exec(personDB, insert_stmt, NULL, NULL, &error) == SQLITE_OK)
-        
-        
-        
-        {
-        
+        if (sqlite3_exec(personDB, insert_stmt, NULL, NULL, &error) == SQLITE_OK) {
             NSLog(@"Person added.");
             
             Person *person = [[Person alloc] init];
@@ -79,18 +76,56 @@
 }
 
 - (IBAction)displayButton:(id)sender {
+    
+    sqlite3_stmt *statement;
+    
+    if (sqlite3_open([dbPathString UTF8String], &personDB) == SQLITE_OK) {
+        [arrayOfPerson removeAllObjects];
+        
+        NSString *querySql = [NSString stringWithFormat:@"SELECT * FROM PERSONS"];
+        const char* query_sql = [querySql UTF8String];
+        if (sqlite3_prepare(personDB, query_sql, -1, &statement, NULL)==SQLITE_OK) {
+            while (sqlite3_step(statement)==SQLITE_ROW) {
+                NSString *name = [[NSString alloc] initWithUTF8String:(const char*)sqlite3_column_text(statement, 1)];
+                NSString *ageString = [[NSString alloc] initWithUTF8String:(const char*)sqlite3_column_text(statement, 2)];
+                
+                Person *person = [[Person alloc] init];
+                
+                [person setName:name];
+                [person setAge:[ageString intValue]];
+                [arrayOfPerson addObject:person];
+            }
+            
+        }
+    }
+    [[self myTableView] reloadData];
 }
 
 - (IBAction)deleteButton:(id)sender {
+    [[self myTableView]setEditing:!self.myTableView.editing
+                         animated:YES];
+    
 }
 
-- (IBAction)displayListButton:(id)sender {
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        Person *p = [arrayOfPerson objectAtIndex:indexPath.row];
+        
+        [self deleteData:[NSString stringWithFormat:@"Delete from persons where name is '%s'", [p.name UTF8String]]];
+        [arrayOfPerson removeObjectAtIndex:indexPath.row];
+        
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
 }
 
-- (IBAction)deletePersonButton:(id)sender {
-}
-
-- (IBAction)displayPersonButton:(id)sender {
+-(void)deleteData:(NSString *)deleteQuery
+{
+    char *error;
+    
+    if (sqlite3_exec(personDB, [deleteQuery UTF8String], NULL, NULL, &error)==SQLITE_OK) {
+        NSLog(@"Person deleted.");
+    }
 }
 
 #pragma mark - TableView Setup
@@ -121,6 +156,13 @@
     cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", aPerson.age];
     
     return cell;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesBegan:touches withEvent:event];
+    [[self ageField] resignFirstResponder];
+    [[self nameField] resignFirstResponder];
 }
 
 @end
